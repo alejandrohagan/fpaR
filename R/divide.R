5b826953a564576698b89f93b9ca799c631b77c2
alejandrohagan <alejandro.hagan@outlook.com>
2023-06-25 17:14:40 -0500

    add new divide function and updated documention

---
+++ R/divide.R
@@ -0,0 +1,28 @@
+
+#' Divide function with error handling for divide by zero or NA
+#'
+#' @param numerator numeric numerator
+#' @param denominator numeric denominator
+#' @param alternative_result alternative results if divide by 0 can by numeric or character but beware of class coercion
+#'
+#' @return
+#' @export
+#'
+#' @examples
+divide <- function(numerator,denominator,alternative_result=NA_integer_){
+
+
+  temp <- `/`(numerator,denominator)
+
+  inf_vec <- which(is.infinite(temp)|is.na(temp))
+
+  if(sum(inf_vec,na.rm=TRUE)>0){
+    cli::cli_alert_info("FYI: Inf or NA values detected")
+  }
+
+
+  temp <- replace(temp,inf_vec,alternative_result)
+  return(temp)
+
+}
+
