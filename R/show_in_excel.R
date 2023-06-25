84851b431c04825bc9fb1d1812da5f0cfa5365ed
alejandrohagan <alejandro.hagan@outlook.com>
2023-06-25 10:34:00 -0500

    update namespace and manual pages

--- R/show_in_excel.R
+++ R/show_in_excel.R
@@ -1,12 +1,19 @@
 
-library(tidyverse)
+#' Converts dataframe to csv file and opens up in excel
+#'
+#' @param .data dataframe in tibble or dataframe format
+#'
+#' @return a csv file
+#' @export
+#'
+#' @examples
+#' ggplot2::diamonds %>% show_in_excel()
 show_in_excel <- function(.data){
-.data=diamonds
+
 # supporting arguments
 rows<- base::dim(.data)[1]
 cols <- base::dim(.data)[2]
 obj_class <- base::class(.data)
-
 
 
 # validation checks
@@ -20,20 +27,22 @@ obj_class <- base::class(.data)
       )
     )
 
-
   if(rows>1e6){
     cli::cli_alert_danger("Row exceeds excel limits -- will not print more than 1M rows")
   } else{
-  cli::cli_alert("data contains {rows} rows and {cols} columns")
-}
+  cli::cli_alert("data contains {rows} row(s) and {cols} column(s)")
+  }
+
+# create temp file path to be used later
   temp <- base::paste0(base::tempfile(),".csv")
 
-  utils::write.csv(.data,file = temp)
+  #sve teh doc df as a csv file
+utils::write.csv(.data,file = temp)
 
+ #open up the csv file
   fs::file_show(path=temp)
 
+  #alert that successful
 cli::cli_alert_success("success: temp file @{temp}")
 }
 
-read
-diamonds %>% show_in_excel()
