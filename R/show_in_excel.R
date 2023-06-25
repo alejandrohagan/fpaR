04b1919d79e4ec82389e04db91794c5b95bc4075
alejandrohagan <alejandro.hagan@outlook.com>
2023-06-25 10:27:07 -0500

    updated cli and included assertthat args

--- R/show_in_excel.R
+++ R/show_in_excel.R
@@ -1,22 +1,39 @@
 
-
+library(tidyverse)
 show_in_excel <- function(.data){
+.data=diamonds
+# supporting arguments
+rows<- base::dim(.data)[1]
+cols <- base::dim(.data)[2]
+obj_class <- base::class(.data)
 
-  rows<- dim(.data)[1]
-  cols <- dim(.data)[1]
 
-  assertthat:class(.data) %in% c("tbl_df", "tbl","data.frame"),msg="data obj must be tibble or dataframe")
+
+# validation checks
+  purrr::map(.x=obj_class, function(.x)
+    assertthat::assert_that(
+      dplyr::if_else(
+        .x %in% c("tbl_df", "tbl","data.frame")
+        ,TRUE
+        ,FALSE)
+      ,msg = "function only supports tibbles or data.frame objects"
+      )
+    )
+
 
   if(rows>1e6){
     cli::cli_alert_danger("Row exceeds excel limits -- will not print more than 1M rows")
-  }
-
+  } else{
+  cli::cli_alert("data contains {rows} rows and {cols} columns")
+}
   temp <- base::paste0(base::tempfile(),".csv")
 
   utils::write.csv(.data,file = temp)
 
   fs::file_show(path=temp)
+
+cli::cli_alert_success("success: temp file @{temp}")
 }
 
-
+read
 diamonds %>% show_in_excel()
