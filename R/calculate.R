60f491bb1332485675fdf775d78e36ff4db007b0
alejandrohagan <alejandro.hagan@outlook.com>
2023-06-28 23:20:14 -0500

    path forward on filter criteria

--- R/calculate.R
+++ R/calculate.R
@@ -1,17 +1,26 @@
 
-.fn="sum"
-.data=diamonds
 
 calculate <- function(.data,.fn,var,rows,cols,...){
-var="price"
-  expr_fn=rlang::expr(sum)
+
+  expr_fn=rlang::expr(.fn)
+  expr_filter=rlang::expr(...)
 
   .data %>%
-    group_by(cut,color) %>%
-    summarise(sum=eval(expr_fn)(!!sym(var)))
+      filter(eval(expr_filter)) %>%
+    group_by(pick({{rows}}),pick({{cols}}))%>%
+    summarise(out=eval(expr_fn)({{var}}))%>%
+    pivot_wider(names_from={{cols}},values_from = out)
 
 
 }
+calculate(diamonds,.fn=min,var=price,rows=c(cut,clarity),cols=color,color=="D",cut=="Fair")
 
+## way to have list or argument get seperately expressed and evaluated
 
+test <- list(sum,mean) %>% map(.,rlang::expr)
 
+eval(pluck(test,1))(1:10)
+test %>% purrr::list_flatten()[1]
+out <- rlang::expr(min)
+
+eval(out)(1:10)
