29a1164edb38db9b7908d899280756df4c8f60fa
alejandrohagan <alejandro.hagan@outlook.com>
2023-06-29 17:13:41 -0500

    updated filter logic

--- R/calculate.R
+++ R/calculate.R
@@ -1,26 +1,41 @@
 
 
-calculate <- function(.data,.fn,var,rows,cols,...){
+calculate <- function(.data,.fn,var,rows,cols,filter){
 
   expr_fn=rlang::expr(.fn)
-  expr_filter=rlang::expr(...)
+ filter_exp <- expr(filter)
 
-  .data %>%
-      filter(eval(expr_filter)) %>%
+.data %>%
+    eval(filter_exp) %>%
     group_by(pick({{rows}}),pick({{cols}}))%>%
     summarise(out=eval(expr_fn)({{var}}))%>%
     pivot_wider(names_from={{cols}},values_from = out)
 
 
 }
-calculate(diamonds,.fn=min,var=price,rows=c(cut,clarity),cols=color,color=="D",cut=="Fair")
+
+
+calculate(diamonds,.fn=min,var=price,rows=cut,cols=color,filter(.,color=="D",cut=="Fair"))
 
 ## way to have list or argument get seperately expressed and evaluated
 
-test <- list(sum,mean) %>% map(.,rlang::expr)
+test <- list(cut=="Fair",color=="D") %>% map(.,rlang::expr)
 
-eval(pluck(test,1))(1:10)
-test %>% purrr::list_flatten()[1]
-out <- rlang::expr(min)
+pluck(test,1)(1:10)
+
+map(test,~pluck(.x)(500:1000))
 
 eval(out)(1:10)
+
+arg <- 'color=="D"'
+
+diamonds %>%
+  filter(eval(expr(color=="D"&cut=="Fair")))
+
+
+
+
+full_out <- expr(filter(.,color=="D",cut=="Fair"))
+
+diamonds %>%
+eval(envir = .,full_out)
