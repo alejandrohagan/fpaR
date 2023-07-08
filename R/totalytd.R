539f9a0b9c13ac347e9f6c70b8afbbaa9d00a77e
alejandrohagan <alejandro.hagan@outlook.com>
2023-07-07 23:27:08 -0500

    updated totalytd

--- R/totalytd.R
+++ R/totalytd.R
@@ -1,19 +1,32 @@
 
-
 TOTALYTD(
   SUM(InternetSales_USD[SalesAmount_USD])
   ,DateTime[DateKey]
   , ALL('DateTime')
   , "6/30"
-  )
+)
+
 library(tidyverse)
 
-totalytd
-fpaR::contoso_fact_sales %>%
-  mutate(DateKey=mdy(DateKey)) %>%
-group_by(DateKey) %>%
-  summarise(sum=sum(UnitCost)) %>%
-  group_by(year=year(DateKey)) %>%
-  mutate(totalytd=cumsum(sum)) %>%
-  ungroup() %>%
-  select(-c(year,sum))
+totalytd <- function(.data,dim,date,fn,start_date,end_date){
+
+
+  .data %>%
+    filter(
+      {{date}}>{{start_date}}
+      ,{{date}}<{{end_date}}
+           ) %>%
+    group_by({{date}}) %>%
+    summarise(sum=sum({{dim}})) %>%
+    group_by(year=year(date)) %>%
+    mutate(totalytd_=cumsum(sum)) %>%
+    ungroup() %>%
+    select(-c(year,sum))
+}
+
+
+df <- fpaR::contoso_fact_sales %>%
+  mutate(date=mdy(DateKey))
+
+df %>% totalytd(dim=SalesQuantity,date=date)
+
