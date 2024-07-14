2048c6f9f25e39e19d0c116ce2aa62fb429601e5
alejandrohagan <alejandro.hagan@outlook.com>
2024-07-13 22:30:46 -0500

    removed new vs. returning customers function

--- tests/testthat/test-cohort.R
+++ tests/testthat/test-cohort.R
@@ -2,15 +2,16 @@
 
 ## create tests
 
-
+library(dplyr)
+library(testthat)
 # test data set
-dat <- crossing(
-  date = seq.Date(from = ymd("2020-01-01"), to = ymd("2024-01-01"), by = "day"),
+dat <- tidyr::crossing(
+  date = seq.Date(from = lubridate::ymd("2020-01-01"), to = lubridate::ymd("2024-01-01"), by = "day"),
   customer = rep(letters[1:17], 86)
 ) |>
-  mutate(purchases = runif(24854))
+  dplyr::mutate(purchases = runif(24854))
 
-test_that("Tibble input returns tibble output", {
+testthat::test_that("Tibble input returns tibble output", {
   testthat::expect_s3_class(
     dat |> make_cohort_tbl(id_var = customer, date_var = date, time_unit = "week", period_label = TRUE),
     "data.frame"
@@ -18,20 +19,20 @@ test_that("Tibble input returns tibble output", {
 })
 
 
-test_that("time_unit validate", {
+testthat::test_that("time_unit validate", {
   testthat::expect_error(
     dat |> fpaR::make_cohort_tbl(id_var = customer, date_var = date, time_unit = "weekly", period_label = TRUE)
   )
 })
 
 
-test_that("date_var validation", {
+testthat::test_that("date_var validation", {
   testthat::expect_error(
     dat |> fpaR::make_cohort_tbl(id_var = customer, date_var = purchases, time_unit = "week", period_label = TRUE)
   )
 })
 
-test_that("Period label validation", {
+testthat::test_that("Period label validation", {
   testthat::expect_error(
     dat |> fpaR::make_cohort_tbl(id_var = customer, date_var = date, time_unit = "week", period_label = 0)
   )
