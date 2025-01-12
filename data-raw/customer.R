9f8d5b34b3dcdb9279fd6a87c5a0d6cf07147a11
alejandrohagan <alejandro.hagan@outlook.com>
2025-01-12 13:06:13 -0600

    wip -- print method

--- data-raw/customer.R
+++ data-raw/customer.R
@@ -2,6 +2,9 @@
 
 
 
+
+
+
 customer <- readr::read_csv("data-raw/customer.csv",name_repair = janitor::make_clean_names)
 
 usethis::use_data(customer, overwrite = TRUE)
--- data-raw/functions.R
+++ data-raw/functions.R
@@ -1,37 +1,15 @@
 ## code to prepare `functions` dataset goes here
 
+library(tidyverse)
 
 
-tibble::tribble(
-  ~"short_name", ~"description", ~"shift", ~"aggregate", ~"compare",
-  "YoY", "Full Year over Year", "", "", "X",
-  "YTD", "Year-to-Date", "", "X", "",
-  "PYTD", "Prior Year-to-Date amount", "X", "X", "",
-  "YoYTD", "Current Year-to-Date over Prior Year-to-Date", "X", "X", "X",
-  "YTDOPY", "Year-to-Date over Full Previous Year", "X", "X", "X",
-  "PYMAT", "Previous Year Moving Annual Total", "X", "X", "X",
-  "QoQ", "Full Quarter over Quarter", "", "", "X",
-  "QTD", "Quarter-to-Date", "", "X", "",
-  "PQTD", "Prior Quarter-to-Date", "X", "X", "",
-  "QOQTD", "Quarter-over-Quarter-to-Date", "X", "X", "X",
-  "QTDOPQ", "Quarter-to-Date over Full Previous Quarter", "X", "X", "X",
-  "MTD", "Month-to-Date", "", "X", "",
-  "MoM", "Full Month over Full Month", "", "", "X",
-  "MoMTD", "Current Month-to-Date over Prior Month-to-Date", "X", "X", "X",
-  "PMTD", "Prior Month's MTD amount", "X", "X", "",
-  "MTDOPM", "Month-to-Date over Full Previous Month", "X", "X", "X",
-  "WTD", "Week-to-Date", "", "X", "",
-  "WoW", "Full Week over Full Week", "", "", "X",
-  "WoWTD", "Current Week-to-Date over Prior Week-to-Date", "X", "X", "X",
-  "PWTD", "Prior Week-to-Date", "X", "X", "",
-  "WTDOPW", "Week-to-Date over Full Previous Week", "X", "X", "X",
-  "MAT", "Moving Annual Total of Previous X Days", "X", "X", "",
-  "ATD","cumlaitve total from inception to date","","x","",
-  "DoD", "Full Day over Full Day", "", "", "X",
+file <- here::here("data-raw","functions.ods")
 
-) |> gt::gt()
+functions <- readODS::read_ods(file,skip = 1) |>
+  select(fn_name,short_name,description,method) |>
+  mutate(
+    fn_name_lower=stringr::str_to_lower(fn_name)
+  )
 
 
-
-
-
+usethis::use_data(functions,overwrite = TRUE)
